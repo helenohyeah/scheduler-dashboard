b52a47b1899d18690dd06e1280f0c6131451d3c3
helenohyeah <ouyang.helen@gmail.com>
2020-11-05 09:37:58 -0500

    Create and render Loading component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,21 @@
+// LIBRARIES
 import React, { Component } from "react";
-
 import classnames from "classnames";
 
+// COMPONENTS
+import Loading from "./Loading";
+
 class Dashboard extends Component {
+  state = {
+    loading: true
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
