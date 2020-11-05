fffd5f0f8807d7e22df7d3ece1320f661259ce17
helenohyeah <ouyang.helen@gmail.com>
2020-11-05 10:15:58 -0500

    Toggle Panel view when selected

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -31,27 +31,44 @@ const data = [
 ]; 
 
 class Dashboard extends Component {
+
+  // DECLARE STATE AND INITIAL VALUES
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
+  
+  // TOGGLE PANEL FOCUS WHEN WHEN SELECTED
+  selectPanel (id) {
+    this.setState({
+      focused: id === this.state.focused ? null : id
+    });
+   }
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
     
+    // SHOW LOADING
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(panel => {
-      return (
-        <Panel
-          key={panel.id}
-          id={panel.id}
-          label={panel.label}
-          value={panel.value}
-        />
-      );
-    });
+    // FILTER DATA THEN CREATE PANELS
+    const panels = data
+      .filter(panel => this.state.focused === null || this.state.focused === panel.id)
+      .map(panel => {
+        return (
+          <Panel
+            key={panel.id}
+            id={panel.id}
+            label={panel.label}
+            value={panel.value}
+            onSelect={e => this.selectPanel(panel.id)}
+          />
+        );
+      });
 
     return (
       <main className={dashboardClasses}>
